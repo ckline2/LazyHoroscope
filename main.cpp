ab217bc297851b0d39c6c6fa3234a4c9e2268a25
ckline2 <ckline2@mail.csuchico.edu>
2018-09-19 16:54:18 -0700

    Calculating number of letters in firstname

--- main.cpp
+++ main.cpp
@@ -7,14 +7,17 @@ using namespace std;
 int main()
 {
   //declare variables
-  string firstname;
-  string lastname;        
+    string firstname;
+    string lastname;
+    int lucky;
   //get user input
-  cout<<"Whats your first name?"<<endl;
-  cin>>firstname;
-  cout<<"Whats your last name?"<<endl;
-  cin>>lastname;
+    cout<<"Whats your first name?"<<endl;
+    cin>>firstname;
+    cout<<"Whats your last name?"<<endl;
+    cin>>lastname;
   //tell fortune
-  cout<<"Welcome, "<<firstname[0]<<lastname[0]<<", here is your fortune... "<<endl;
-  return 0;
+    cout<<"Welcome, "<<firstname[0]<<lastname[0]<<", here is your fortune... "<<endl;
+    cout<<"The variable has "<<firstname.length()<<" letters."<<endl;
+    cout<<"Your lucky number is "<<firstname.length()<<endl;
+  return 0; 
 }
