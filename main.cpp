45ed2c6fe304a76cbf494ccf41a257b0a4046821
ckline2 <ckline2@mail.csuchico.edu>
2018-09-19 16:15:11 -0700

    First/last name

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//Authors:
+//Authors:Charlie Kline
 #include<iostream>
 #include<string>
 
@@ -7,9 +7,13 @@ using namespace std;
 int main()
 {
   //declare variables
-
+  string firstname;
+  string lastname;        
   //get user input
-
+  cout<<"Whats your first name?"<<endl;
+  cin>>firstname;
+  cout<<"Whats your last name?"<<endl;
+  cin>>lastname;
   //tell fortune
 
   return 0;
