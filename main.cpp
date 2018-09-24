66c80c61089ee98c815522fc70d2ba8fc07872c4
ckline2 <ckline2@mail.csuchico.edu>
2018-09-24 16:52:27 -0700

    Part 2 of lazyHoroscope lab

--- main.cpp
+++ main.cpp
@@ -19,5 +19,15 @@ int main()
     cout<<"Welcome, "<<firstname[0]<<lastname[0]<<", here is your fortune... "<<endl;
     cout<<"The variable has "<<firstname.length()<<" letters."<<endl;
     cout<<"Your lucky number is "<<firstname.length()<<endl;
+    if (firstname[0] == 'A' || firstname[0] == 'a' || firstname[0] == 'E' || firstname[0] == 'e' || firstname[0] == 'I' || firstname[0] == 'i' || firstname[0] == 'O' || firstname[0] == 'o' || firstname[0] == 'U' || firstname[0] == 'u'){
+        cout<<"Destined to be famous"<<endl;
+                                                                                                                }    
+    else {
+        cout<<"Keep a low profile"<<endl;    
+         } 
+    if (lastname[0] == 'A' || lastname[0] == 'a' || lastname[0] == 'E' || lastname[0] == 'e' || lastname[0] == 'I' || lastname[0] == 'i' || lastname[0] == 'O' || lastname[0] == 'o' || lastname[0] == 'U' || lastname[0] == 'u'){
+        cout<<"You have already met your true love"<<endl;
+                                                                                                         }
+    cout<<"Have a good day!"<<endl;
   return 0; 
 }
